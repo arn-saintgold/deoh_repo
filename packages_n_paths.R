0994e1666a2ee0ecc8847af84883ba25b0028684
Arnaldo Santoro <arnaldoantoro@unive.it>
2024-02-01 15:57:03 +0100

    emo_csv_statistics runs fine

--- packages_n_paths.R
+++ packages_n_paths.R
@@ -1,6 +1,9 @@
+#install.packages("data.table", type = "source", repos = "https://Rdatatable.gitlab.io/data.table")
 library(tidyverse)
 library(data.table)
 library(ggplot2)
 
 data_dir = file.path('data', 'processed')
 plot_dir = file.path('output')
+
+emotions = c("anger", "trust", "surprise", "disgust", "joy", "sadness", "fear", "anticipation")
