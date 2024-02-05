b8dfed48bcb3abfbf88d223febc1695b9359c606
Arnaldo Santoro <arnaldoantoro@unive.it>
2024-02-05 12:33:01 +0100

    added chi^2 on any emotion

--- scripts/chisq_test_emo_trustworthyness.R
+++ scripts/chisq_test_emo_trustworthyness.R
@@ -1,10 +1,30 @@
-# Chi square test on independence between emotions and source trustworthyness 
+# Chi square test on independence between emotions and source trustworthiness 
 
 source('packages_n_global_variables.R')
-
 emo_csv <- fread(emo_csv_path)
-
 emotions <- emotions[c(2,5,8,3,7,6,1,4)]
+
+
+# Chi square test of independence between presence of any emotion and 
+#   trustworthiness, and Cramer's V for impact size
+
+q_r_has_emo <- emo_csv[,.(
+  counts=sum(has_emotion*1), 
+  negativeCounts = .N-sum(has_emotion*1)
+  ), 
+  by = Is_questionable]
+
+M<-q_r_has_emo[,.(Is_questionable,counts, negativeCounts)]%>%t()
+colnames(M)<-M[1,]
+Mn<-matrix(as.numeric(M[2:3,]),ncol = 2)
+rownames(Mn)<-rownames(M[2:3,])
+colnames(Mn)<-colnames(M)
+chisq.result <- chisq.test(Mn)
+print(chisq.result)
+print(confintr::ci_cramersv(chisq.result))
+
+# Chi square test of independence between each singular emotion presence and 
+# trustworthiness, and Cramer's V for impact size
 
 emo_in_comments <- emo_csv[emotiveness>0, .(counts=colSums(.SD), negativeCounts = .N-colSums(.SD), emo=names(.SD)), by=Is_questionable, .SDcols = paste0('has_',emotions)]
 
@@ -22,7 +42,12 @@ for (e in emo_in_comments[,emo]%>%unique()){
   print(chisq.result)
   print(confintr::ci_cramersv(chisq.result))
   my_cramer<-confintr::ci_cramersv(chisq.result)
-  chi_cram_row <- c(e%>%str_remove('has_')%>%str_to_title(), round(chisq.result$statistic,digit=2), chisq.result$p.value, round(my_cramer$estimate,3), round(my_cramer$interval[1],3), round(my_cramer$interval[2],3))
+  chi_cram_row <- c(e%>%str_remove('has_')%>%str_to_title(), 
+                    round(chisq.result$statistic,digit=2), 
+                    chisq.result$p.value, 
+                    round(my_cramer$estimate,3), 
+                    round(my_cramer$interval[1],3), 
+                    round(my_cramer$interval[2],3))
   chi_cram <- rbind(chi_cram,chi_cram_row)
   
 }
