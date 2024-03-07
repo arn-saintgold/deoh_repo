6c9a1c1da34bd9eaf97251a0393ce712c93e6d27
arn-saintgold <arnaldo.santoro@unive.it>
2024-03-07 10:06:01 +0100

    chi square code fix

--- scripts/chisq_test_emo_trustworthyness.R
+++ scripts/chisq_test_emo_trustworthyness.R
@@ -19,7 +19,7 @@ colnames(M)<-M[1,]
 Mn<-matrix(as.numeric(M[2:3,]),ncol = 2)
 rownames(Mn)<-rownames(M[2:3,])
 colnames(Mn)<-colnames(M)
-chisq.result <- chisq.test(Mn)
+chisq.result <- chisq.test(Mn, correct = F)
 print(chisq.result)
 print(confintr::ci_cramersv(chisq.result))
 
@@ -32,17 +32,19 @@ chi_cram <- data.frame()
 for (e in emo_in_comments[,emo]%>%unique()){
   message(e)
   curr_emo <- emo_in_comments[emo == e]
+  this_n <- emo_csv[get(e)>0 & n_emotions>0, .N]
   M<-curr_emo[,.(Is_questionable,counts, negativeCounts)]%>%t()
   colnames(M)<-M[1,]
   Mn<-matrix(as.numeric(M[2:3,]),ncol = 2)
   rownames(Mn)<-rownames(M[2:3,])
   colnames(Mn)<-colnames(M)
   
-  chisq.result <- chisq.test(Mn)
+  chisq.result <- chisq.test(Mn, correct=F)
   print(chisq.result)
   print(confintr::ci_cramersv(chisq.result))
   my_cramer<-confintr::ci_cramersv(chisq.result)
-  chi_cram_row <- c(e%>%str_remove('has_')%>%str_to_title(), 
+  chi_cram_row <- c(e%>%str_remove('has_')%>%str_to_title(),
+                    this_n,
                     round(chisq.result$statistic,digit=2), 
                     chisq.result$p.value, 
                     round(my_cramer$estimate,3), 
@@ -51,6 +53,6 @@ for (e in emo_in_comments[,emo]%>%unique()){
   chi_cram <- rbind(chi_cram,chi_cram_row)
   
 }
-names(chi_cram)<-c('Emotion', '$\\chi^2$', 'p.value', "Cramer's V", "Cramer's V low", "Cramer's V high" )
+names(chi_cram)<-c('Emotion','N', '$\\chi^2$', 'p.value', "Cramer's V", "Cramer's V low", "Cramer's V high" )
 
 print.xtable(xtable(chi_cram), row.names = F,include.rownames=FALSE)
--- scripts/processing/emo_signal_detection/emo_csv_statistics.R
+++ scripts/processing/emo_signal_detection/emo_csv_statistics.R
@@ -11,16 +11,16 @@ only_emo <- merged_comments[,.SD, .SDcols = c(emotions, "csv_id")]%>%copy()
 only_emo[,emotiveness := .(sum(anger, trust, surprise, disgust, joy, sadness, fear, anticipation)), by=csv_id]
 only_emo<-only_emo[,.(csv_id, emotiveness)]
 emo_csv<-merge(merged_comments, only_emo)
-# 'has_emotion' column contains information on whether any emotion was detected in the comment
-emo_csv[,has_emotion := emotiveness != 0]
 
 # prettify the is_questionable and has_emotion column 
 emo_csv[,Is_questionable:= factor(is_questionable, labels = c("Reliable","Questionable"))]
-emo_csv[,Has_emotion := factor(has_emotion, labels = c("No Signal", "Emotive"))]
 
 # We want to know which emotion is significatively present in each comment
 # Eight 'has_X' indicator variables are added - with X as pluthik emotion - to indicate significative presence of emotion
 emo_csv[, paste0("has_",emotions) := lapply(.SD, function(x)  x>qnorm(.975)), .SDcols = emotions]
 emo_csv[, n_emotions := rowSums(.SD), .SDcols = paste0("has_",emotions) ]
+# 'has_emotion' column contains information on whether any emotion was detected in the comment
+emo_csv[,has_emotion := n_emotions> 0]
+emo_csv[,Has_emotion := factor(has_emotion, labels = c("No Signal", "Emotive"))]
 
 fwrite(emo_csv, file.path(data_dir,"emo_csv_statistics.gz"))
