039b4cf19d92cde9634279e1e44a24d095e31659
arn-saintgold <arnaldo.santoro@unive.it>
2024-04-23 22:43:23 +0200

    added one more test and comments to dyad test

--- scripts/multiple_emotion_undersampling_test.R
+++ scripts/multiple_emotion_undersampling_test.R
@@ -1,6 +1,6 @@
 source('packages_n_global_variables.R')
 
-N_RESAMPLINGS = 10#10e3
+N_RESAMPLINGS = 10e4
 
 # get comments from users with defined leaning and at least 8 comments
 usr_emo_lean <- fread(usr_emo_lean_path)
@@ -10,17 +10,6 @@ names(usr_lean)[which(names(usr_lean) == "is_questionable")] = "is_usr_questiona
 emo_csv_usr_lean <- merge(emo_csv, usr_lean)
 
 # compute dyads in users' comments
-
-Dyad_DF <- data.table(Dyad = usr_dyad_cols, 
-                      e1 = c(rep('trust',7), rep('joy',6), rep('fear',5), rep('anger',4), rep('anticipation', 3), rep('sadness',2), 'surprise'),
-                      e2 = c(c("joy","fear","surprise","sadness","anticipation","anger","disgust"),
-                             c("anticipation","fear","anger","surprise","disgust","sadness"),
-                             c("surprise","sadness","anticipation","disgust","anger"),
-                             c("disgust","anticipation","sadness","surprise"),
-                             c("disgust","sadness","surprise"),
-                             c("surprise","disgust"),
-                             "disgust"
-                             ))
 
 emo_csv_usr_lean[, dyad_love := (has_trust + has_joy)==2]
 emo_csv_usr_lean[, dyad_submission := (has_trust + has_fear)==2]
@@ -91,7 +80,7 @@ usr_dyads <- emo_csv_usr_lean[,.(
   unbelief = mean(dyad_unbelief),
   is_usr_questionable = mean(is_usr_questionable) ), by=Nome_Utente]
 
-usr_dyad_cols = dyad_cols%>%str_remove('dyad_')
+usr_dyad_cols = comments_dyad_cols%>%str_remove('dyad_')
 
 # perform a permutation test
 
@@ -101,6 +90,22 @@ comments_dyads_means <- emo_csv_usr_lean[,lapply(.SD, mean),
 usr_dyads_means <- usr_dyads[,lapply(.SD, mean),
                              by = is_usr_questionable,
                              .SDcols = usr_dyad_cols]
+
+fwrite(usr_dyads_means, file.path(data_dir,"usr_dyad_means.csv"))
+fwrite(usr_dyads_means, file.path(data_dir,"usr_dyad_means.tsv"), sep = "\t")
+
+# reference table for Dyads
+Dyad_DF <- data.table(Dyad = usr_dyad_cols, 
+                      e1 = c(rep('trust',7), rep('joy',6), rep('fear',5), rep('anger',4), rep('anticipation', 3), rep('sadness',2), 'surprise'),
+                      e2 = c(c("joy","fear","surprise","sadness","anticipation","anger","disgust"),
+                             c("anticipation","fear","anger","surprise","disgust","sadness"),
+                             c("surprise","sadness","anticipation","disgust","anger"),
+                             c("disgust","anticipation","sadness","surprise"),
+                             c("disgust","sadness","surprise"),
+                             c("surprise","disgust"),
+                             "disgust"
+                      ))
+
 # mean dyad value for Misinformation Prone Users
 mip_dyads_means = usr_dyads_means[is_usr_questionable == 1]
 map_dyads_means = usr_dyads_means[is_usr_questionable == 0]
@@ -109,7 +114,9 @@ n_MIp = usr_dyads[is_usr_questionable == 1,.N]
 n_MAp = usr_dyads[is_usr_questionable == 0,.N]
 
 usr_dyad_distribution = data.table()
+pb = txtProgressBar(min = 0, max = N_RESAMPLINGS, initial = 0) 
 for (i in 1:N_RESAMPLINGS){
+  setTxtProgressBar(pb,i)
   undersampled_map_dyads <- usr_dyads[is_usr_questionable == 0][
     sample(1:n_MAp, n_MIp),lapply(.SD, mean),
     by = is_usr_questionable,
@@ -117,20 +124,46 @@ for (i in 1:N_RESAMPLINGS){
   undersampled_map_dyads[,is_usr_questionable :=NULL]
   usr_dyad_distribution = rbindlist(list(usr_dyad_distribution,undersampled_map_dyads))
 }
+close(pb)
 usr_dyad_undersampled_means<-usr_dyad_distribution[,lapply(.SD, mean),.SDcols = usr_dyad_cols]
 usr_dyad_undersampled_sd<-usr_dyad_distribution[,lapply(.SD, sd),.SDcols = usr_dyad_cols]
 
 n_signigicant_dyads = 0
+
 for (dyad in usr_dyad_cols){
+  
   original_dyad_mean = mip_dyads_means[,get(dyad)]
   sampled_dyad_mean = usr_dyad_undersampled_means[,get(dyad)]
   sampled_dyad_sd = usr_dyad_undersampled_sd[,get(dyad)]
-  dyad_is_significant <- sampled_dyad_mean - sampled_dyad_sd*2 < original_dyad_mean & sampled_dyad_mean + sampled_dyad_sd*2
-  msg <- str_flatten(c(dyad," (",Dyad_DF[Dyad == dyad, e1]," + ",Dyad_DF[Dyad == dyad, e2],") ",if_else(dyad_is_significant==T, ' IS ', ' is NOT '),'significant.'))
+  dyad_is_significant <- original_dyad_mean > sampled_dyad_mean + sampled_dyad_sd*2
+  msg <- str_flatten(c(dyad," (",Dyad_DF[Dyad == dyad, e1]," + ",Dyad_DF[Dyad == dyad, e2],") ",if_else(dyad_is_significant==T, ' IS ', ' is NOT '),'significantly higher in MIp.'))
   if(dyad_is_significant){
-    #message(msg)
-    n_signigicant_dyads = n_signigicant_dyads +1
+    message(msg)
+    n_signigicant_dyads = n_signigicant_dyads + 1
   }
-  else{message(msg)}
 }
+#submission (trust + fear)  IS significantly higher in MIp.
+#dominance (trust + anger)  IS significantly higher in MIp.
+#frozenness (fear + anger)  IS significantly higher in MIp.
+
+for (dyad in usr_dyad_cols){
+  
+  original_dyad_mean = mip_dyads_means[,get(dyad)]
+  sampled_dyad_mean = usr_dyad_undersampled_means[,get(dyad)]
+  sampled_dyad_sd = usr_dyad_undersampled_sd[,get(dyad)]
+  dyad_is_significant <- original_dyad_mean < sampled_dyad_mean - sampled_dyad_sd*2
+  msg <- str_flatten(c(dyad," (",Dyad_DF[Dyad == dyad, e1]," + ",Dyad_DF[Dyad == dyad, e2],") ",if_else(dyad_is_significant==T, ' IS ', ' is NOT '),'significantly lower in MIp.'))
+  if(dyad_is_significant){
+    message(msg)
+    n_signigicant_dyads = n_signigicant_dyads + 1
+  }
+}
+#delight (joy + surprise)  IS significantly lower in MIp.
+#pessimism (anticipation + sadness)  IS significantly lower in MIp.
+
+what_is_greater <- as.data.table(mip_dyads_means > map_dyads_means)
+fwrite(what_is_greater, file.path(data_dir,"what_is_greater.csv"))
+fwrite(what_is_greater, file.path(data_dir,"what_is_greater.tsv"), sep = '\t')
 n_signigicant_dyads
+
+N_RESAMPLINGS
